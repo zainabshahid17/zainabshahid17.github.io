be540a34afed7c3b3b49e1bf33170a59a7f28f80
Zainab Noor Shahid <zainab.shahid1744@gmail.com>
2024-09-10 15:49:18 +0500

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1199479763"
+    serviceWorkerVersion: "3848594471"
   }
 });
